2459181acc09b10d63c4016f80882aca6191c1b4
Fuat Akdemir <akdemir_fuat@hotmail.com>
2022-02-04 06:43:08 +0300

    Array Couples added.

--- EvenPair.py
+++ EvenPair.py
@@ -1,14 +1,14 @@
 """
 EN
-Verilen string ifadesinde ardışık sayılarda 2 sayı çift ise True döndüren program
-Örnek: "35rt7gsgeth81egeg44rtrtb" -> 44, ardışık 2 çift sayıdır ve True değerini döndürür.
-Örnek: "39tefq25kl4lw2139" -> Ardışık sayılarda 2 çift sayı olmadığından false döndürür.
-Örnek: "to34lh741hcbp341ro874" -> 874 ardışık sayı 8 ve 4 çift sayı içerdiğinden True döndürür.
+Program that returns True if 2 consecutive numbers are even in the given string expression.
+Example: "35rt7gsgeth81egeg44rtrtb" -> 44 is 2 consecutive even numbers and returns True.
+Example: "39tefq25kl4lw2139" -> Returns false as there are no 2 even numbers in consecutive numbers.
+Example: "to34lh741hcbp341ro874" -> Returns True because 874 consecutive numbers contain 8 and 4 even numbers.
 """
 
 """
 TR
-Verilen string ifade içinde ardışık sayılar içinde 2 tane rakamın çift olması durumunda True dönen program
+Verilen string ifade içinde ardışık sayılar içinde 2 tane rakamın çift olması durumunda True dönen program.
 Örnek: "35rt7gsgeth81egeg44rtrtb" -> 44 ardışık 2 çift sayıdır ve True döndürür.
 Örnek: "39tefq25kl4lw2139" -> Ardışık sayılar içinde 2 adet çift sayı olmadığı için false döndürür.
 Örnek: "to34lh741hcbp341ro874" -> 874 ardışık sayılar içinde 8 ve 4 çift sayılar olduğundan True döndürür.
